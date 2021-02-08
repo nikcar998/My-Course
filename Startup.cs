44ecbf56225f0402732483d56cc6de132e08380b
nikcar <nikcar34@gmail.com>
2021-02-08 17:51:24 +0100

    Razor Pages and controllers

--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,7 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
